d4868961182ef40f36c3fc5085cd2fc2a650d480
Mooshieblob1 <blob@mooshieblob.com>
2024-10-31 20:01:53 +0800

    Refactor PageTransition component

--- src/lib/components/layout/PageTransition.svelte
+++ src/lib/components/layout/PageTransition.svelte
@@ -1,13 +1,13 @@
 <!-- src/lib/components/layout/PageTransition.svelte -->
 <script lang="ts">
-  import { fly, fade } from 'svelte/transition';
-  import { cubicInOut } from 'svelte/easing';
-  
-  // Get current URL to use as a key for the transition
-  import { page } from '$app/stores';
-  
-  // Transition duration
-  const duration = 200;
+	import { fly, fade } from 'svelte/transition';
+	import { cubicInOut } from 'svelte/easing';
+
+	// Get current URL to use as a key for the transition
+	import { page } from '$app/stores';
+
+	// Transition duration
+	const duration = 200;
 </script>
 
 <!-- FILEPATH: /home/mooshieblob/comfyui-site.github.io/src/lib/components/layout/PageTransition.svelte -->
@@ -28,14 +28,7 @@
   </PageTransition>
 -->
 {#key $page.url.pathname}
-  <div
-    in:fly={{ y: 20, duration, delay: duration, easing: cubicInOut }}
-    out:fade={{ duration }}
-  >
-    <slot />
-  </div>
+	<div in:fly={{ y: 20, duration, delay: duration, easing: cubicInOut }} out:fade={{ duration }}>
+		<slot />
+	</div>
 {/key}
-
-
-  git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
